54b0eb71b10a05d5974c9ee30ca4a41164bfa67c
jtrauer <james.trauer@monash.edu>
2024-01-09 20:46:32 +1100

    New structure for process values

--- semimech/emu_renewal/process.py
+++ semimech/emu_renewal/process.py
@@ -1,35 +1,63 @@
-from typing import List
+from typing import Tuple
 import numpy as np
-import pandas as pd
 from scipy.interpolate import CubicSpline
 
 
-def get_interp_vals_over_model_time(
-    req_x_vals: List[float],
-    req_y_vals: List[float], 
+def get_linear_interp_func(
+    req_x_vals: np.array,
+    req_y_vals: np.array, 
 ) -> callable:
     """Linear interpolation at requested values at regular intervals over simulation period.
 
-    # Args:
-    #     req: Requested values
-    #     n_times: Length of simulation, which the request will be evenly spread over
+    Args:
+        req_x_vals: Requested x-values
+        req_y_vals: Requested y-values
 
-    # Returns:
-    #     Values for each of the n time values of the simulation period
+    Returns:
+        The function
     """
     return lambda x: np.interp(x, req_x_vals, req_y_vals)
 
-def get_spline_values_over_model_time(
-    req_x_vals: List[float],
-    req_y_vals: List[float], 
+def get_spline_interp_func(
+    req_x_vals: np.array,
+    req_y_vals: np.array, 
 ) -> callable:
-    """Another standard interpolation option, works better.
+    """Another standard interpolation option,
+    which has advantages of being continuous with continuous gradient.
 
-    # Args:
-    #     req: Requested values
-    #     n_times: Length of simulation, which the request will be evenly spread over
+    Args:
+        req_x_vals: Requested x-values
+        req_y_vals: Requested y-values
 
-    # Returns:
-    #     Values for each of the n time values of the simulation period
+    Returns:
+        The function
     """
     return CubicSpline(req_x_vals, req_y_vals)
+
+def get_cos_points_link(
+    coords_a: Tuple[float], 
+    coords_b: Tuple[float],
+) -> callable:
+    if coords_a[0] == coords_b[0]:
+        raise ValueError('Same x-value for both submitted coordinates')
+    period_adj = np.pi / (coords_b[0] - coords_a[0])
+    amplitude = coords_b[1] - coords_a[1]
+    def cos_link(x):
+        return (-np.cos((x - coords_a[0]) * period_adj) + 1.0) / 2.0 * amplitude + coords_a[1]
+    return cos_link
+
+def get_piecewise_cosine(x_vals, y_vals):
+    coords = list(zip(x_vals, y_vals))
+    def piecewise_cosine_func(x):
+        start_cond = x < x_vals[0]
+        mid_conds = [x_vals[i] <= x < x_vals[i + 1] for i in range(len(coords) - 1)]
+        end_cond = x >= x_vals[-1]
+        conds = [start_cond] + mid_conds + [end_cond]
+
+        start_func = lambda x: y_vals[0]
+        mid_funcs = [get_cos_points_link(coords[i], coords[i + 1]) for i in range(len(coords) - 1)]
+        end_func = lambda x: y_vals[-1]
+        funcs = [start_func] + mid_funcs + [end_func]
+        
+        return np.piecewise(x, conds, funcs)
+    return np.vectorize(piecewise_cosine_func)
