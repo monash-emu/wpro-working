b05734dbfc5668f627f6c1524af5aa986898010f
James Trauer <james.trauer@monash.edu>
2024-02-01 10:19:15 +1100

    Do similar instantiation for density distribution

--- renew/emu_renewal/distributions.py
+++ renew/emu_renewal/distributions.py
@@ -4,16 +4,14 @@ from scipy.stats import gamma
 
 
 class GammaDens():
-    def __init__(self, req_mean, req_sd):
-        self.req_mean = req_mean
-        self.req_sd = req_sd
-        self.parameters = self.get_gamma_params_from_mean_sd()
+    def __init__(self):
+        pass
 
-    def get_gamma_params_from_mean_sd(self):
-        var = self.req_sd ** 2.0
-        scale = var / self.req_mean
-        a = self.req_mean / scale
-        return {'a': a, 'scale': scale}
+    def get_gamma_params_from_mean_sd(self, req_mean, req_sd):
+        var = req_sd ** 2.0
+        scale = var / req_mean
+        a = req_mean / scale
+        self.parameters = {'a': a, 'scale': scale}
     
     def get_gamma_densities_from_params(self, n_times):
         return np.diff(gamma.cdf(range(n_times + 1), **self.parameters))
