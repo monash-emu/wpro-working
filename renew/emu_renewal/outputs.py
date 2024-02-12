fce1845603474daa8f0fa46fbbe4f1967c7c4ef3
James Trauer <james.trauer@monash.edu>
2024-02-12 16:12:07 +1100

    Switch to filling to self to make code simpler and more terse

--- renew/emu_renewal/outputs.py
+++ renew/emu_renewal/outputs.py
@@ -16,18 +16,14 @@ def plot_spaghetti(cases, targets, proc, suscept, r, margins, titles):
 def plot_uncertainty_patches(cases, select_data, proc, suscept, r, margins, titles):
     fig = make_subplots(rows=2, cols=2, shared_xaxes=True, vertical_spacing=0.05, horizontal_spacing=0.05, subplot_titles=titles)
     fig.update_layout(margin=margins, height=600, showlegend=False)
-    x_vals = proc.index
+    x_vals = cases.index
     fig.add_trace(go.Scatter(x=select_data.index, y=select_data, mode="markers"), row=1, col=1)
-    fig.add_trace(go.Scatter(x=x_vals, y=cases[0.05], line={"width": 0.0}), row=1, col=1)
-    fig.add_trace(go.Scatter(x=x_vals, y=cases[0.95], fill="tonexty", mode="none"), row=1, col=1)
-    fig.add_trace(go.Scatter(x=x_vals, y=cases[0.5], line={"width": 1.0}), row=1, col=1)
-    fig.add_trace(go.Scatter(x=x_vals, y=suscept[0.05], line={"width": 0.0}), row=1, col=2)
-    fig.add_trace(go.Scatter(x=x_vals, y=suscept[0.95], fill="tonexty", mode="none"), row=1, col=2)
-    fig.add_trace(go.Scatter(x=x_vals, y=suscept[0.5], line={"width": 1.0}), row=1, col=2)
-    fig.add_trace(go.Scatter(x=x_vals, y=r[0.05], line={"width": 0.0}), row=2, col=1)
-    fig.add_trace(go.Scatter(x=x_vals, y=r[0.95], fill="tonexty", mode="none"), row=2, col=1)
-    fig.add_trace(go.Scatter(x=x_vals, y=r[0.5], line={"width": 1.0}), row=2, col=1)
-    fig.add_trace(go.Scatter(x=x_vals, y=proc[0.05], line={"width": 0.0}), row=2, col=2)
-    fig.add_trace(go.Scatter(x=x_vals, y=proc[0.95], fill="tonexty", mode="none"), row=2, col=2)
-    fig.add_trace(go.Scatter(x=x_vals, y=proc[0.5], line={"width": 1.0}), row=2, col=2)
+    fig.add_trace(go.Scatter(x=list(x_vals) + list(x_vals[::-1]), y=list(cases[0.05]) + list(cases[0.95][::-1]), fill="toself"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=x_vals, y=cases[0.5]), row=1, col=1)
+    fig.add_trace(go.Scatter(x=list(x_vals) + list(x_vals[::-1]), y=list(suscept[0.05]) + list(suscept[0.95][::-1]), fill="toself"), row=1, col=2)
+    fig.add_trace(go.Scatter(x=x_vals, y=suscept[0.5]), row=1, col=2)
+    fig.add_trace(go.Scatter(x=list(x_vals) + list(x_vals[::-1]), y=list(r[0.05]) + list(r[0.95][::-1]), fill="toself"), row=2, col=1)
+    fig.add_trace(go.Scatter(x=x_vals, y=r[0.5]), row=2, col=1)
+    fig.add_trace(go.Scatter(x=list(x_vals) + list(x_vals[::-1]), y=list(proc[0.05]) + list(proc[0.95][::-1]), fill="toself"), row=2, col=2)
+    fig.add_trace(go.Scatter(x=x_vals, y=proc[0.5]), row=2, col=2)
     return fig
