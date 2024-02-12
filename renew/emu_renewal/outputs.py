41a1ccc7e9e9ec3ef56608e52ef1d41cc31ef155
James Trauer <james.trauer@monash.edu>
2024-02-13 09:24:13 +1100

    Simplify patch plotting with additional function

--- renew/emu_renewal/outputs.py
+++ renew/emu_renewal/outputs.py
@@ -18,16 +18,17 @@ def get_plotly_area_from_df(df, columns, colour):
     return go.Scatter(x=x_vals, y=y_vals, line={"width": 0.0, "color": colour}, fill="toself")
 
 
+def add_ci_patch_to_plot(fig, df, colour, row, col):
+    x_vals = df.index.to_list() + df.index[::-1].to_list()
+    fig.add_trace(get_plotly_area_from_df(df, columns=[0.05, 0.95], colour=colour), row=row, col=col)
+    fig.add_trace(go.Scatter(x=x_vals, y=df[0.5], line={"color": colour}), row=row, col=col)
+
+
 def plot_uncertainty_patches(cases, targets, proc, suscept, r, margins, titles, colours):
     fig = make_subplots(rows=2, cols=2, shared_xaxes=True, vertical_spacing=0.05, horizontal_spacing=0.05, subplot_titles=titles)
-    x_vals = cases.index
     fig.add_trace(go.Scatter(x=targets.index, y=targets, mode="markers"), row=1, col=1)
-    fig.add_trace(get_plotly_area_from_df(cases, columns=[0.05, 0.95], colour=colours[0]), row=1, col=1)
-    fig.add_trace(go.Scatter(x=x_vals, y=cases[0.5], line={"color": colours[0]}), row=1, col=1)
-    fig.add_trace(get_plotly_area_from_df(suscept, columns=[0.05, 0.95], colour=colours[1]), row=1, col=2)
-    fig.add_trace(go.Scatter(x=x_vals, y=suscept[0.5], line={"color": colours[1]}), row=1, col=2)
-    fig.add_trace(get_plotly_area_from_df(r, columns=[0.05, 0.95], colour=colours[2]), row=2, col=1)
-    fig.add_trace(go.Scatter(x=x_vals, y=r[0.5], line={"color": colours[2]}), row=2, col=1)
-    fig.add_trace(get_plotly_area_from_df(proc, columns=[0.05, 0.95], colour=colours[3]), row=2, col=2)
-    fig.add_trace(go.Scatter(x=x_vals, y=proc[0.5], line={"color": colours[3]}), row=2, col=2)
+    add_ci_patch_to_plot(fig, cases, colours[0], 1, 1)
+    add_ci_patch_to_plot(fig, suscept, colours[1], 1, 2)
+    add_ci_patch_to_plot(fig, r, colours[2], 2, 1)
+    add_ci_patch_to_plot(fig, proc, colours[3], 2, 2)
     return fig.update_layout(margin=margins, height=600, showlegend=False)
