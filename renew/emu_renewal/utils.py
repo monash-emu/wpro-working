ae2bafa9686d121b5a1a2de69f991c6589d9311c
James Trauer <james.trauer@monash.edu>
2024-02-16 09:16:33 +1100

    Adjust date format for markdown

--- renew/emu_renewal/utils.py
+++ renew/emu_renewal/utils.py
@@ -1,4 +1,4 @@
 def format_date_for_str(date):
     ord_excepts = {1: "st", 2: "nd", 3: "rd"}
     ordinal = ord_excepts.get(date.day % 10, "th")
-    return date.strftime(f"%-d{ordinal} %B %Y")
+    return date.strftime(f"%#d<sup>{ordinal}</sup> %B %Y")
