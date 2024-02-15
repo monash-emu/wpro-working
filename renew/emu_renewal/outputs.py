b957897cbe2cd5b422d7fa5a21a829f03db72525
James Trauer <james.trauer@monash.edu>
2024-02-16 09:52:56 +1100

    Improve consistency of order of outputs

--- renew/emu_renewal/outputs.py
+++ renew/emu_renewal/outputs.py
@@ -1,30 +1,33 @@
 from plotly import graph_objects as go
 from plotly.subplots import make_subplots
 
+PANEL_SUBTITLES = ["cases", "susceptibles", "R", "transmission potential"]
+MARGINS = {m: 20 for m in ["t", "b", "l", "r"]}
+
 
 def get_quantiles_from_spaghetti(spaghetti, quantiles):
     return spaghetti.quantile(quantiles, axis=1).T
 
 
-def get_standard_four_subplots(titles):
+def get_standard_four_subplots():
     return make_subplots(
         rows=2,
         cols=2,
         shared_xaxes=True,
         vertical_spacing=0.05,
         horizontal_spacing=0.05,
-        subplot_titles=titles,
+        subplot_titles=PANEL_SUBTITLES,
     )
 
 
-def plot_spaghetti(cases, targets, proc, suscept, r, margins, titles):
-    fig = get_standard_four_subplots(titles)
+def plot_spaghetti(cases, targets, suscept, r, proc):
+    fig = get_standard_four_subplots()
     fig.add_trace(go.Scatter(x=targets.index, y=targets, mode="markers"), row=1, col=1)
     fig.add_traces(cases.plot().data, rows=1, cols=1)
-    fig.add_traces(proc.plot().data, rows=2, cols=1)
     fig.add_traces(suscept.plot().data, rows=1, cols=2)
-    fig.add_traces(r.plot().data, rows=2, cols=2)
-    return fig.update_layout(margin=margins, height=600)
+    fig.add_traces(r.plot().data, rows=2, cols=1)
+    fig.add_traces(proc.plot().data, rows=2, cols=2)
+    return fig.update_layout(margin=MARGINS, height=600)
 
 
 def get_area_from_df(df, columns, colour):
@@ -39,11 +42,11 @@ def add_ci_patch_to_plot(fig, df, colour, row, col):
     fig.add_trace(go.Scatter(x=x_vals, y=df[0.5], line={"color": colour}), row=row, col=col)
 
 
-def plot_uncertainty_patches(cases, targets, proc, suscept, r, margins, titles, colours):
-    fig = get_standard_four_subplots(titles)
+def plot_uncertainty_patches(cases, targets, suscept, r, proc, colours):
+    fig = get_standard_four_subplots()
     fig.add_trace(go.Scatter(x=targets.index, y=targets, mode="markers"), row=1, col=1)
     add_ci_patch_to_plot(fig, cases, colours[0], 1, 1)
     add_ci_patch_to_plot(fig, suscept, colours[1], 1, 2)
-    add_ci_patch_to_plot(fig, proc, colours[2], 2, 1)
-    add_ci_patch_to_plot(fig, r, colours[3], 2, 2)
-    return fig.update_layout(margin=margins, height=600, showlegend=False)
+    add_ci_patch_to_plot(fig, r, colours[2], 2, 1)
+    add_ci_patch_to_plot(fig, proc, colours[3], 2, 2)
+    return fig.update_layout(margin=MARGINS, height=600, showlegend=False)
