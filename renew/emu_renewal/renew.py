28bc50aa91b8ed7ab0180e8d3bfd50fe7fbd1523
jtrauer <james.trauer@monash.edu>
2024-02-19 22:16:11 +1100

    Comment most of remaining model

--- renew/emu_renewal/renew.py
+++ renew/emu_renewal/renew.py
@@ -1,10 +1,13 @@
-from typing import List
+from typing import Union, Optional
 from typing import NamedTuple
 from jax import lax, vmap
 from jax import numpy as jnp
 from datetime import datetime
 
+from summer2.utils import Epoch
+
 from emu_renewal.process import cosine_multicurve, sinterp
+from emu_renewal.distributions import Dens
 from emu_renewal.utils import format_date_for_str, round_sigfig
 
 
@@ -21,50 +24,95 @@ class ModelResult(NamedTuple):
 
 
 class RenewalModel:
-    def __init__(self, population, start, end, run_in_req, proc_update_freq, dens_obj, window_len, epoch=None):        
-        self.epoch = epoch
+    def __init__(
+        self, 
+        population: float, 
+        start: Union[datetime, int], 
+        end: Union[datetime, int], 
+        run_in_req: int, 
+        proc_update_freq: int, 
+        dens_obj: Dens, 
+        window_len: int, 
+        epoch: Optional[Epoch],
+    ):
+        """Standard renewal model object.
 
+        Args:
+            population: Starting population value
+            start: Start time for the analysis period (excluding run-in)
+            end: End time for the analysis period
+            run_in_req: Duration of additional period preceding the analysis period
+            proc_update_freq: Frequency with with the vairable process is updated
+            dens_obj: Generation time distribution
+            window_len: How far to look back in calculating the renewal process
+            epoch: Reference epoch for calculating dates. Defaults to None.
+        """
+
+        # Times
+        self.epoch = epoch
         self.start = self.process_time_req(start)
         self.end = self.process_time_req(end)
         self.simulation_start = self.start - run_in_req
         self.model_times = jnp.arange(self.simulation_start, self.end + 1)
-        self.fixed_param_desc = (
+        self.description = (
+            "\n### Fixed parameters\n"
             f"The main analysis period runs from {format_date_for_str(start)} "
             f"to {format_date_for_str(end)}, "
             f"with a preceding run in period of {run_in_req} days. "
         )
 
+        # Population
         self.pop = population
-        self.fixed_param_desc += (
+        self.description += (
             f"The starting model population is {round_sigfig(population / 1e6, 2)} million persons. "
         )
-        
+
+        # Window        
         self.window_len = window_len
-        self.fixed_param_desc += (
+        self.description += (
             "The preceding time window for renewal calculations "
             f"is set to {window_len} days. "
         )
 
+        # Process
         self.x_proc_vals = jnp.arange(self.end, self.start, -proc_update_freq)[::-1]
         self.x_proc_data = sinterp.get_scale_data(self.x_proc_vals)
         process_start = int(self.x_proc_vals[0])
         self.run_in = process_start - self.simulation_start
         if run_in_req != self.run_in:
-            self.fixed_param_desc += (
+            self.description += (
+                "\n### Variable process\n"
                 "Because the analysis period is not an exact multiple "
                 "of the duration of a process interval, "
                 f"the run-in period is extended from {run_in_req} days "
                 f"to {self.run_in} days. "
             )
 
+        # Generation
         self.dens_obj = dens_obj
-        self.fixed_param_desc += f"\n{self.dens_obj.get_desc()}"
+        self.description += f"\n{self.dens_obj.get_desc()}"
 
+        # Seeding
         self.seed_x_vals = jnp.linspace(self.simulation_start, self.start, 3)
         self.start_seed = 0.0
         self.end_seed = 0.0
+        self.describe_seed_func()
 
-    def process_time_req(self, req):
+    def process_time_req(
+        self, 
+        req: Union[datetime, int],
+    ) -> int:
+        """Sort out a user requested date.
+
+        Args:
+            req: The request
+
+        Raises:
+            ValueError: If neither date nor int
+
+        Returns:
+            The request converted to int according to the model's epoch
+        """
         msg = "Time data type not supported"
         if isinstance(req, int):
             return req
@@ -73,10 +121,35 @@ class RenewalModel:
         else:
             raise ValueError(msg)
 
-    def seed_func(self, t, seed):
+    def seed_func(
+        self, 
+        t: float, 
+        seed_peak: float,
+    ) -> float:
+        """See describe_seed_func
+
+        Args:
+            t: Model time
+            seed_peak: Peak seeding rate 
+
+        Returns:
+            Seeding rate at the requested time
+        """
         x_vals = sinterp.get_scale_data(jnp.array(self.seed_x_vals))
-        y_vals = sinterp.get_scale_data(jnp.array([self.start_seed, jnp.exp(seed), self.end_seed]))
+        y_vals = sinterp.get_scale_data(jnp.array([self.start_seed, jnp.exp(seed_peak), self.end_seed]))
         return cosine_multicurve(t, x_vals, y_vals)
+    
+    def describe_seed_func(self):
+        self.description += (
+            "\n### Seeding\n   "
+            f"The seeding process scales up from a value of {self.start_seed} "
+            "at the start of the run-in period to it's peak value "
+            "through the first half of the run-in, "
+            f"and then decays back to {self.end_seed} at the end of the run-in. "
+            "This is implemented using a half-cosine function "
+            "that is translated and scaled to reach zero gradient at each of the "
+            "three specified points (start, peak, end). "
+        )
 
     def fit_process_curve(self, y_proc_vals):
         cos_func = vmap(cosine_multicurve, in_axes=(0, None, None))
@@ -108,7 +181,7 @@ class RenewalModel:
 
     def get_model_desc(self):
         renew_desc = (
-            "\n\n### Renewal process\n"
+            "## Renewal process\n"
             "Calculation of the renewal process "
             "consists of multiplying the incidence values for the preceding days "
             "by the reversed generation time distribution values. "
@@ -125,7 +198,7 @@ class RenewalModel:
         )
 
         non_mech_desc = (
-            "\n\n### Non-mechanistic process\n"
+            "## Non-mechanistic process\n"
             "The time values corresponding to the submitted process values "
             "are set to be evenly spaced throughout the simulation period. "
             "Next, a continuous function of time was constructed from "
@@ -140,12 +213,4 @@ class RenewalModel:
 
     def get_full_desc(self):
 
-        seed_desc = (
-            "\n\n### Seeding\n"
-            "Seeding was achieved by interpolating using a cosine function. "
-            f"The number of seeded cases scaled from {self.start_seed} at time {self.seed_x_vals[0]} "
-            "to the peak value at half way through the burn-in period "
-            f"back to {self.end_seed} at the end of the burn-in period ({self.seed_x_vals[-1]}). "
-        )
-
-        return self.dens_obj.get_desc() + self.get_model_desc() + seed_desc
+        return self.dens_obj.get_desc() + self.get_model_desc()
