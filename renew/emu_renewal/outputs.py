fbcabee4ea7b7471dbb5fb06e2c8a7c81df172e9
James Trauer <james.trauer@monash.edu>
2024-02-20 11:26:24 +1100

    Comment half the outputs file

--- renew/emu_renewal/outputs.py
+++ renew/emu_renewal/outputs.py
@@ -1,13 +1,34 @@
+from typing import List
 import numpy as np
 import pandas as pd
 from plotly import graph_objects as go
 from plotly.subplots import make_subplots
 
+from estival.sampling.tools import SampleIterator
+
+from .renew import RenewalModel
+
 PANEL_SUBTITLES = ["cases", "susceptibles", "R", "transmission potential"]
 MARGINS = {m: 20 for m in ["t", "b", "l", "r"]}
 
 
-def get_spaghetti_from_params(model, params, model_func):
+def get_spaghetti_from_params(
+    model: RenewalModel, 
+    params: SampleIterator, 
+    model_func: callable,
+) -> pd.DataFrame:
+    """Run parameters through the model to get outputs.
+
+    Args:
+        model: The renewal model
+        params: The parameter sets to feed through the model
+        model_func: The model function to run the parameters through
+
+    Returns:
+        Dataframe with index of model times and multiindexed columns,
+            with first level being the output name and second the parameter set
+            by chain and iteration
+    """
     index_names = model.epoch.index_to_dti(model.model_times)
     column_names = pd.MultiIndex.from_product([params.index.map(str), PANEL_SUBTITLES])
     spaghetti = pd.DataFrame(index=index_names, columns=column_names)
@@ -18,7 +39,23 @@ def get_spaghetti_from_params(model, params, model_func):
     return spaghetti.sort_index(axis=1, level=0)
 
 
-def get_quant_df_from_spaghetti(model, spaghetti, quantiles):
+def get_quant_df_from_spaghetti(
+    model: RenewalModel, 
+    spaghetti: pd.DataFrame, 
+    quantiles: List[float],
+) -> pd.DataFrame:
+    """Calculate requested quantiles over spaghetti created
+    in previous function.
+
+    Args:
+        model: The renewal model
+        spaghetti: The output of get_spaghetti_from_params
+        quantiles: The quantiles at which to make the calculations
+
+    Returns:
+        Dataframe with index of model times and multiindexed columns,
+            with first level being the output name and second the quantile
+    """
     index_names = model.epoch.index_to_dti(model.model_times)
     column_names = pd.MultiIndex.from_product([PANEL_SUBTITLES, quantiles])
     quantiles_df = pd.DataFrame(index=index_names, columns=column_names)
